272a9cf5096ca874367f1ab922990ea145714fdf
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-06-14 01:31:48 +0300

    feat: Built post  Searching functionality

--- src/components/ui/SearchResults.tsx
+++ src/components/ui/SearchResults.tsx
@@ -1,7 +1,22 @@
+import { Models } from "appwrite";
 import React from "react";
+import Loader from "./Loader";
+import { searchPosts } from "@/lib/appwrite/services";
+import GridPostList from "./GridPostList";
 
-const SearchResults = () => {
-  return <div>SearchResults</div>;
+type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.DocumentList<Models.Document> | undefined;
+};
+
+const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+  if (isSearchFetching) return <Loader />;
+
+  if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  }
+
+  return <p className="text-light-4 mt-10 text-center w-full">No results found</p>;
 };
 
 export default SearchResults;
